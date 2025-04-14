b456ab25c49f37560bebd0fe6cadd35eeb0a7c74
Sofien Laghouanem <Sofienlaghouanem@gmail.com>
2025-04-14 19:16:16 +0100

    .

--- BackUp/Front/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
-  testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
-  transform: {
-    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.json" }],
-  },
-};
