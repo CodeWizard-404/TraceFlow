d70b26b15d9370d2cb72801ed41ab450c338cdb5
Ghaith OTHMANI <mekluless@gmail.com>
2025-04-16 11:52:05 +0100

    translation files done
    french and english files for the whole website are done

--- Mobile/lib/utils/constants.dart
+++ Mobile/lib/utils/constants.dart
@@ -1,2 +1,2 @@
 
-const String baseUrl = 'http://192.168.1.16:5000/api';
+const String baseUrl = 'http://192.168.0.101:5000/api';
--- keycloak/Front/vite.config.ts
+++ keycloak/Front/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://192.168.1.26:5000',
+        target: 'http://192.168.0.101:5000',
         changeOrigin: true,
         secure: false,
       },
