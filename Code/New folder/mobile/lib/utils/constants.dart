0a2a2ef016dafa8e25340faaad8bd4b86eb1fc07
Sofien Laghouanem <Sofienlaghouanem@gmail.com>
2025-06-01 19:10:49 +0100

    .

--- Code/New folder/mobile/lib/utils/constants.dart
+++
@@ -1,6 +0,0 @@
-const String baseUrl = 'http://192.168.1.23:5000/api';
-const String keycloakUrl = 'https://4ae9-102-157-229-80.ngrok-free.app';
-const String realm = 'TraceFlow';
-const String clientId = 'traceflow-backend';
-const String clientSecret = '9FQJDVvEU3MZvB9CGxuM50IcTVOapED7';
-const String redirectUri = 'https://4ae9-102-157-229-80.ngrok-free.app/realms/TraceFlow/broker/google/endpoint';
