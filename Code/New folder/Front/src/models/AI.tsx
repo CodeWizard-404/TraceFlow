bd34d94bb3d42e0a90e20b59ed119ffdd6aa0ad9
Sofien Laghouanem <Sofienlaghouanem@gmail.com>
2025-11-01 18:36:45 +0100

    .

--- Code/New folder/Front/src/models/AI.tsx
+++
@@ -1,9 +0,0 @@
-export interface AIConfig {
-    configID: string;
-    modelName: string;
-    maxOptimizeRoute: number;
-    timesheetMaxSuggestions: number;
-    supervisorId: string | null;
-    createdAt: string;
-    updatedAt: string;
-}
--- Code/New folder/Front/src/models/ReceiptStub.tsx
+++
@@ -1,9 +0,0 @@
-import ReceiptStubStatus from "./Enum/ReceiptStubStatus";
-
-interface ReceiptStub {
-    stubID: string;
-    bookID: string;
-    status: ReceiptStubStatus
-}
-
-export default ReceiptStub;
